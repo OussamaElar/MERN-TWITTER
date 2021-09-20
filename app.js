ebaf7854511bd303a252353bb78606d304e8cd70
OussamaElar <oussama.elar@gmail.com>
2021-09-20 09:18:39 -0400

    twitter

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+const path = require("path");
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const db = require("./config/keys").mongoURI;
 const app = express();
 
-//mongodb+srv://admin:<password>@mern.nsxy7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
-
-app.get("/", (req, res) => {
-      res.send("Hello");
-})
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
-app.listen(port , () => (console.log(`lestning on port ${port}`)))
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(passport.initialize());
+require("./config/passport")(passport); 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+      mangoURI: "mongodb+srv://admin:0Y9WQeuVKpyA7Xsl@mern.nsxy7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      secretOrKey: "secret"
+}
+
+
